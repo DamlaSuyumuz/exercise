220c9d3b0df4d0c762041aabdb58f4f914da368c
Damla <163167718+DamlaSuyumuz@users.noreply.github.com>
2024-03-15 16:20:07 +0300

    Create arifmetic.py

---
+++ arifmetic.py
@@ -0,0 +1,12 @@
+sub = int(num1) - int(num2)
+
+# Произведение двух чисел
+mul = float(num1) * float(num2)
+
+# Частное двух чисел
+div = float(num1) / float(num2)
+
+print(f'Сумма чисел {num1} и {num2} равна {sum_}')
+print(f'Разница чисел {num1} и {num2} равна {sub}')
+print(f'Произведение чисел {num1} и {num2} равна {mul}')
+print(f'Частное чисел {num1} и {num2} равна {div}')
